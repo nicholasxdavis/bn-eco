3d0d3757800942db98e352e94b8cdb1972bf3aac
blacnova <147455788+nicholasxdavis@users.noreply.github.com>
2025-09-17 20:29:25 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF--8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blacnova Dashboard</title>
+      <link rel="icon" href="https://www.blacnova.net/img/bn_orange.png" type="image/png">
     <style>
         body, html {
             margin: 0;
